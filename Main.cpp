8ab6d59d1b6c9cb7681debcfe403b83cbd71901a
Kelley Kelley <mike1555@colorado.edu>
2018-11-20 10:27:50 -0700

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -14,7 +14,7 @@ struct letterNode {
   char letter;
   //the type of word it is if its a stop word, noun, verb, adjective
   int type;
-  //the score that determines what to do with that word
+  //the score that determines what to do with that word, the different nouns, different adj
   int identifier;
   //this is the score the word has, for example negative will be a -1
   int score;
@@ -24,8 +24,9 @@ struct letterNode {
 
 //this is the struct used in the response trie
 struct wordNode {
-  //the word held at this noded
+  //the word held at this node
   string word;
+  //trieIdentifier, trieType, endScore all correlate to letterNode's type, identifier, score
   //this is one of the variables to help determine what type of sentence it is, and therefore whether or not
   //to use this trie
   int trieIdentifier;
@@ -37,17 +38,27 @@ struct wordNode {
   vector<wordNode> adjacents;
 };
 
-class wordsTrie {
+class keywordsTrie {
 public:
-  bool isInTrie(String findWord) {
-
+  //
+  bool findWord(String wordToFind) {
+    //return false if word not found
+    //return true if word found, and update the
+    //score, type, identifier
   }
   void addWord(String word) {
-
+    //adds a word to the trie
   }
+  void removeWord(String word) {
+
+  } //each of the above methods will split the word into characters itself
+  //these are the variables to pass to the responsesTrie
+  int score;
+  int type;
+  int identifier;
 };
 
 class responsesTrie {
 public:
-  
+
 };
