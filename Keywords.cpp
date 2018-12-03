521daba416add29aabbd72f0b7e08625a0f85255
Lara <laragchunko@gmail.com>
2018-12-03 15:28:54 -0700

    Add files via upload

--- Keywords.cpp
+++ Keywords.cpp
@@ -13,24 +13,96 @@ using namespace std;
 
 //constructor
 KeywordsTrie::KeywordsTrie() {
-  readKeywords();
-  Responding responder();
+    start = initializeLetterNode('a');
+    readKeywords();
 }
+
+struct letterNode* KeywordsTrie::initializeLetterNode(char letterToAdd) {
+    letterNode *letter = new letterNode;
+
+    letter->isEnd = false;
+    letter->identifier = letter->type = -1;
+    letter->letter = letterToAdd;
+
+    for(int i = 0; i < ALPHABET_SIZE; i++) {
+        letter->children[i] = NULL;
+    }
+
+    return letter;
+} 
 
 void KeywordsTrie::findWord(string word) {
   //find word then update type and identifier to this words for use by takeInSentence
-  //wordType = the guys you found type
-  //wordIdentifier = the guy you found identifier
+  //wordType = the type of the word you found 
+  //wordIdentifier = the identifier of the word you found 
   //if its type is 7, then use the previous noun data not this words
+
+    letterNode *crawl = start;
+
+    for(int i = 0; i < word.size(); i++) {
+        int index = word[i] - 'a';
+        if(crawl->children[index] == NULL) {
+            wordType = 9;
+            wordIdentifier = 9;
+            return;
+        }
+        crawl = crawl->children[index];
+    }
+
+    if(crawl != NULL && crawl->isEnd) {
+        wordType = crawl->type;
+        wordIdentifier = crawl->identifier;
+    }
+    else {
+        wordType = 9;
+        wordIdentifier = 9;
+    }
 }
 
 //adds a word to the trie and txt file
-void KeywordsTrie::addWord(string word, int i, int t) {
+void KeywordsTrie::addWord(string word, int t, int i) {
+    //check if the word already exists or not
+    findWord(word);
+    if(wordType == t && wordIdentifier == i) return;
 
+    //add word to the trie
+    prepTrie(word, t, i);
+
+    //add word to txtfile
+    /*
+    ofstream myfile;
+    myfile.open("testKeywords.txt", ios_base::app);
+    myfile << word << " " << t << " " << i << "\n";
+    myfile.close();
+    */
+}
+
+//add a similar word
+void KeywordsTrie::synonym(string toAdd, string similar) {
+    findWord(toAdd);
+    if(wordType != 9 && wordIdentifier != 9) return;
+    findWord(similar);
+    addWord(toAdd, wordType, wordIdentifier);  
 }
 
 //remove a word from the keywords trie and txt file
 void KeywordsTrie::removeWord(string word) {
+    findWord(word);
+    if(wordType == 9 && wordIdentifier == 9) return;
+
+    letterNode *crawl = start;
+
+    for(int i = 0; i < word.size(); i++) {
+        int index = word[i]-'a';
+        if(crawl->children[index] == NULL) {
+            return;
+        }
+        crawl = crawl->children[index];
+    }
+
+    crawl->isEnd = false;
+
+    //then remove the word from the file
 
 }
 
@@ -40,9 +112,45 @@ void KeywordsTrie::takeInSentence(string userInput) {
   //to end of sentence structure and its identifier to the end
   //of sentenceIdentifier int
   //findword(first word);
-  //example: sentenceStructore = sentenceStructore*10 + wordType;
+  //example: sentenceStructure = sentenceStructure*10 + wordType;
   //example: sentenceIdentifier = sentenceIdentifier*10 + wordIdentifier;
-  //then once done with every word responder.respond(sentenceStructore, sentenceIdentifier);
+  //then once done with every word responder.respond(sentenceStructure, sentenceIdentifier);
+
+    stringstream ss;
+    ss << userInput;
+    string word;
+    
+    sentenceStructure = 0;
+    sentenceIdentifier = 0;
+
+    while(getline(ss, word, ' ')) {
+        findWord(word);
+        sentenceStructure = sentenceStructure*10 + wordType;
+        sentenceIdentifier = sentenceIdentifier*10 + wordIdentifier;
+    }
+    responder.respond(sentenceStructure, sentenceIdentifier);
+}
+
+void KeywordsTrie::prepTrie(string word, int t, int i) {
+    letterNode *crawl = start;
+
+    for(int i = 0; i < word.size(); i++) {
+        //get ascii value of word - ascii value of a to have an index in range of [0, 26)
+        int index = word[i]-'a';
+
+        //if the first letter of the word does not exist in keywords trie, make a new letterNode
+        if(crawl->children[index] == NULL) {
+            char charOfLetter = word[i];
+            crawl->children[index] = initializeLetterNode(charOfLetter);
+        }
+
+        //move through each letterNode in keywords trie
+        crawl = crawl->children[index];
+    }
+
+    crawl->isEnd = true;
+    crawl->type = t;
+    crawl->identifier = i;
 }
 
 //read the key words from a file
@@ -53,15 +161,33 @@ void KeywordsTrie::readKeywords() {
         //read the file
         string line;
 
-        while(getline(myfile, line)) {
+        while(getline(myfile, line)) { //gives me a line
             stringstream ss;
             ss<<line;
             string word;
+            string input[3] = {};
+            int index = 0;
             while(getline(ss, word, ' ')) {  //this gives me one word
-
-                //if(!isStopWord(word, ignoreWords))
+                input[index] = word;
+                index++;
             }
+            //at 0 = word
+            //at 1 = type
+            //at 2 = identifier
+            prepTrie(input[0], stoi(input[1]), stoi(input[2]));
         }
     }
     myfile.close();
 }
+
+/*
+int main() {
+
+    KeywordsTrie k;
+    k.addWord("bananas", 7, 7);
+
+    return 0;
+}
+*/
+
+
--- Keywords.hpp
+++ Keywords.hpp
@@ -13,16 +13,22 @@ using namespace std;
 //variables to help us and pass ints because ints are much faster to use than a string
 const int NOUN = 1, VERB = 2, ADJECTIVE = 3, ADVERB = 4, MULTIPLIER = 5, QUESTION = 6, PREVIOUS = 7;
 
+const int ALPHABET_SIZE = 26;
+
 //this struct is a letter node used used in the keyword trie
 struct letterNode {
   //the letter at this node
   char letter;
   //the type of word it is if its a stop word, noun, verb, adjective
+    //if it isn't an end letter, the value will be -1
   int type;
   //the score that determines what to do with that word, the different nouns, different adj
+    //if it isn't an end letter, the value will be -1
   int identifier;
   //these are all the next nodes
-  vector<letterNode> adjacents;
+  struct letterNode *children[ALPHABET_SIZE];
+  //the letter is at the end of a word
+  bool isEnd;
 };
 
 class KeywordsTrie {
@@ -30,23 +36,38 @@ public:
   //constructor
   KeywordsTrie();
   //finds a word for use by takeInSentence
-  //unnecisary words will be 0 so they dont affect my sentence computations
+  //unecessary words will be 0 so they dont affect the sentence computations
   void findWord(string word);
   //adds a word to keyword trie and txt file
-  void addWord(string word, int i, int t);
+  void addWord(string word, int t, int i);
   //removes a word from the keyword trie and txt file
   void removeWord(string word);
   //takes in the users response
   void takeInSentence(string userInput);
   //reads in the txt file and puts the words in a trie
   void readKeywords();
+
+  //initialize a letterNode to be used in the Trie
+  struct letterNode *initializeLetterNode(char letterToAdd);
+
+  //first word, the word to add
+  //second word, the word it's similar to
+  void synonym(string toAdd, string similar);
+
+  //helper function for the readKeywords function
+  void prepTrie(string word, int t, int i);
+
+  //the root of the keywords trie
+  struct letterNode *start;
   //int that stores each words type in order
-  int sentenceStructore;
+  int sentenceStructure;
   //int that stores each words identifier in order
   int sentenceIdentifier;
   //word identifier and type for use by find word and takeInSentence
   int wordType;
   int wordIdentifier;
+  //make a responding object for taking in responses and responding
+  Responding responder;
 };
 
 #endif
